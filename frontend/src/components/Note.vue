814e59834af329f7f65da6b55c8e141188ffccc6
choikj129 <ckj9829@naver.com>
2021-11-22 18:18:37 +0900

    Update Note.vue

--- frontend/src/components/Note.vue
+++ frontend/src/components/Note.vue
@@ -1,72 +1,69 @@
 <template>
     <div id="wrapper">
-        <b-button id="add" @click="add">+</b-button>
-        <div class="boxwrap" :key="b" v-for="b in box" :id="b.id" @click="drag(b.id)"
-        :style="{left:b.x+'px', top:b.y+'px', width:b.w+'px', height:b.h+'px'}">
-            <div :style="{width:'100%', height:'20px', position:'absolute'}"></div>
-            <textarea class="box"></textarea>
+        <b-button id="add" @click="add" v-show="display==''">+</b-button>
+        <div id="boxwrap" v-show="display==''">
+            <b-card class="box" :key="i" v-for="(note,i) in notes" :title="note.title" :sub-title="note.date">
+                <b-card-text class="content" v-html="note.content">
+                </b-card-text>
+            </b-card>
         </div>
-
-        <div class="boxwrap" :style="{left:'30px', top:'100px'}">
-            <div :style="{width:'300px', height:'20px', backgroundColor:'red'}"></div>
-            <textarea class="box"
-            :style="{width:'300px', height:'500px'}"></textarea>
+        <div id="modal" v-show="display=='add'">
+            <b-input-group class="mt-3">
+                <b-form-input id="input" placeholder="제목"></b-form-input>
+                <b-input-group-append>
+                    <b-button variant="outline-dark" @click="cancel">X</b-button>
+                </b-input-group-append>
+            </b-input-group>
+            <b-form-textarea id="textarea"></b-form-textarea>
         </div>
-        
     </div>
 </template>
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script>
+import $ from 'jquery'
+
 export default {
     data() {
         return{
-            box : [],
-            x : 0,
-            y : 0
+            notes : [
+                {id:"1",title:"", date:"2020-11-22", content:"test<br>test<br>test<br>test<br>test<br>test<br>test<br>"},
+                {id:"2",title:"마트", date:"2020-11-22", content:"testtest<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>"}
+            ],
+            display : ""
         }
     },
     name : "note",
     methods : {
         add() {
+            this.display = "add"
             // this.box.push({x : "30.00px", y:"30px"})
-            this.axios.post('/api/add',{
-                x : 30,
-                y : 30,
-                w : 100,
-                h : 200
-            }).then(res=>{
-                console.log("success")
-            })
+            // this.axios.post('/api/add',{
+            //     x : 30,
+            //     y : 30,
+            //     w : 100,
+            //     h : 200
+            // }).then(res=>{
+            //     console.log("success")
+            // })
         },
-        drag(id){
-            document.getElementById(id).onmousedown = function mouseDown(e){
-                e = e || window.event
-                if (e.button==0){
-                    this.x = e.clientX;
-                    this.y = e.clientY;
-
-                    document.onmousemove = function mouseMove(e){
-                        e = e || window.event
-
-                        var dx = this.x - e.clientX;
-                        var dy = this.y - e.clientY;
-
-                    }
-                }
-            }
-            
+        cancel(){
+            this.display = ""
         }
     },
     created(){
-        this.axios.get("/api")
-        .then((res)=>{
-            for(var i=0; i<res.data.length; i++){
-                res.data[i]["id"]="box"+res.data[i]["id"]
-                this.box.push(res.data[i])
-            }
-        })
+        // this.axios.get("/api")
+        // .then((res)=>{
+        //     this.notes = res.data
+        // })
     }
+    // beforeCreate(){
+    //     this.axios.get("/api")
+    //     .then((res)=>{
+    //         this.notes = res.data
+    //     })
+    // }
 }
+
 </script>
 <style>
     body {
@@ -77,16 +74,58 @@ export default {
         height: 80px;
         border-radius: 50%;
         background-color: #5F5F4F;
-        position : absolute;
+        position : fixed;
         bottom : 50px;
         right : 50px
     }
-    .boxwrap{
-        position: absolute;
-    }
     .box{
-        position : absolute;
-        background-color: #5CFFEF;
-        resize: both;
+        display: block;
+        background-color: rgba(92, 255, 239, 0.65);
+        width : 300px;
+        height: 150px;
+        /* border-radius: 18px; */
+        margin : 0 auto;
+        margin-top: 10px;
+        box-shadow: 3px 3px 3px 3px gray;
+        /* padding : 0 10px; */
+        overflow:hidden;
+    }
+    .card-body{
+        /* font-family: monospace; */
+        height: inherit;
+        padding-top: 10px;
+    }
+    .title{
+        font: 20px;
+        font-weight: bold;
+    }
+    .date{
+        color: grey;
+    }
+    .content{
+        height: 100%;
+        overflow: hidden;
+        text-overflow:ellipsis;
+        white-space: pre-line;
+    }
+    #modal{
+        width: 600px;
+        height: 800px;
+        margin: 30px auto;
+        border-radius: 18px;
+        background-color: #F3F3CC;
+    }
+    #input{
+        background-color: inherit;
+        width: 400px;
+        border: inherit;
+        
+    }
+    #textarea{
+        resize: none;
+        height: 768px;
+        background-color: inherit;
+        border: inherit;
+        border-top: gray solid 1px;
     }
 </style>
