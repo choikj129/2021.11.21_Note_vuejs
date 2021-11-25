764a7cf31807a4ea898c9d97ea63fa3d5f062e83
choikj129 <ckj9829@naver.com>
2021-11-25 19:00:05 +0900

    2021-11-25
    휴지통

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require("fs");
 const data = JSON.parse(fs.readFileSync(path.resolve(__dirname,"../db.json")));
 var mysql = require('mysql2')
+var moment = require("moment")
 
 var connection = mysql.createConnection({
   host : data.host,
@@ -15,35 +16,53 @@ var connection = mysql.createConnection({
 
 router.get("/", function(req, res){
   connection.query(
-    `select * from note`,
+    `select * from note order by date desc`,
     function(err, result){
-      if(err){
-        console.log(err)
-      }else{
-        res.send(result)
-      }
+      err ? console.log(err) : connection.query(
+        `select * from trash order by date desc`,
+        function(err, result2){
+          err ? console.log(err) : res.send({notes : result, trash : result2})
+        }
+      )
     }
   )
 })
 
-router.post("/add", function(req, res){
-  console.log("add")
-  var x = req.body.x;
-  var y = req.body.y;
-  var w = req.body.w;
-  var h = req.body.h;
+router.get("/add", function(req, res){
+  console.log("add");
+  var date = moment().format("YYYY-MM-DD HH:mm:ss")
+  var title = req.query.title;
+  var content = req.query.content;
   connection.query(
-    `insert into note(x,y,w,h) values(?,?,?,?)`,
-    [x,y,w,h],
+    `insert into note(date,title,content) values(?,?,?)`,
+    [date,title,content],
     function(err){
-      if(err){
-        console.log(err)
-      }else{
-        res.json({})
-      }
+      err ? console.log(err) : res.redirect("/")
     }
   )
 })
 
+router.get("/trash", function(req, res){
+  var id = req.query.id;
+  var date = moment().format("YYYY-MM-DD HH:mm:ss")
+  console.log(id)
+  connection.query(
+    `insert into trash select * from note where id=${id}`,
+    function(err){
+      err ? console.log(err) : connection.query(
+        `delete from note where id=${id}`,
+        function(err){
+          err ? console.log(err) : connection.query(
+            `update trash set date=? where id=?`,
+            [date,id],
+            function(err){
+              err ? console.log(err) : res.redirect("/")
+            }
+          )
+        }
+      )
+    }
+  )
+})
 
 module.exports = router;
--- frontend/src/components/Note.vue
+++ frontend/src/components/Note.vue
@@ -1,9 +1,21 @@
 <template>
     <div id="wrapper">
-        <b-button id="add" @click="add" v-show="display==''">+</b-button>
+        <b-icon icon="trash" class="icon" @click="display='trash'" :style="{top:'0px', right:'0px'}"></b-icon>
+        <b-icon icon="card-list"  class="icon" @click="display=''" :style="{top:'0px', left:'0px'}"></b-icon>
+        <b-icon icon="plus" id="add" @click="display='add'" v-show="display==''"></b-icon>
+        <!-- <b-button id="add" @click="add" v-show="display==''">+</b-button> -->
         <div id="boxwrap" v-show="display==''">
-            <b-card class="box" :key="i" v-for="(note,i) in notes" :title="note.title" :sub-title="note.date">
+            <b-card class="box" :key="i" v-for="(note,i) in notes" :title="note.title" :sub-title="note.date" :id="note.id">
+                <b-icon icon="pencil" class="icon update" :style="{top:'0px', right:'22px'}"></b-icon>
+                <b-icon icon="x" class="icon trash" :style="{top:'0px', right:'0px'}" @click="trash(note.id)"></b-icon>
                 <b-card-text class="content" v-html="note.content">
+                </b-card-text>
+            </b-card>
+        </div>
+        <div v-show="display=='trash'">
+            <b-card class="box" :key="i" v-for="(trash,i) in trashcan" :title="trash.title" :sub-title="trash.date" :id="trash.id">
+                <b-icon icon="x" class="icon del" :style="{top:'0px', right:'0px'}" @click="del"></b-icon>
+                <b-card-text class="content" v-html="trash.content">
                 </b-card-text>
             </b-card>
         </div>
@@ -23,12 +35,14 @@
 import $ from 'jquery'
 
 export default {
+    
     data() {
         return{
             notes : [
                 {id:"1",title:"", date:"2020-11-22", content:"test<br>test<br>test<br>test<br>test<br>test<br>test<br>"},
                 {id:"2",title:"마트", date:"2020-11-22", content:"testtest<br>test<br>test<br>test<br>test<br>test<br>test<br>test<br>"}
             ],
+            trashcan : [],
             display : ""
         }
     },
@@ -36,32 +50,33 @@ export default {
     methods : {
         add() {
             this.display = "add"
-            // this.box.push({x : "30.00px", y:"30px"})
-            // this.axios.post('/api/add',{
-            //     x : 30,
-            //     y : 30,
-            //     w : 100,
-            //     h : 200
-            // }).then(res=>{
-            //     console.log("success")
-            // })
         },
         cancel(){
             this.display = ""
+            var i = $("#input").val()
+            var t = $("#textarea").val()
+            if (i || t){
+                location.href = `/api/add?title=${i}&content=${t}`
+                this.axios.post('/api/add',{
+                    title : i,
+                    content : t
+                }).then(res =>{
+                    this.notes = res.data
+                })
+            }
+        },
+        trash(id){
+            location.href = `/api/trash?id=`+id
         }
+                       
     },
     created(){
-        // this.axios.get("/api")
-        // .then((res)=>{
-        //     this.notes = res.data
-        // })
+        this.axios.get("/api")
+        .then((res)=>{
+            this.notes = res.data.notes
+            this.trashcan = res.data.trash
+        })
     }
-    // beforeCreate(){
-    //     this.axios.get("/api")
-    //     .then((res)=>{
-    //         this.notes = res.data
-    //     })
-    // }
 }
 
 </script>
@@ -76,7 +91,13 @@ export default {
         background-color: #5F5F4F;
         position : fixed;
         bottom : 50px;
-        right : 50px
+        right : 50px;
+        font-size: 30px;
+    }
+    .icon{
+        width : 20px;
+        height: 20px;
+        position : absolute;
     }
     .box{
         display: block;
